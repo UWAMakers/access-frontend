a4b396a43ce5d560d34f235e6e44f7f0ff9a08f8
mrfrase3 <mrfrase3@gmail.com>
2021-04-11 20:35:08 +0800

    move eslint to own file

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-underscore-dangle': 'off',
+    'vue/valid-v-slot': 'off',
+  },
+};
